6eaa490b04aedb8f6d94505afbe075e07d88a96c
wpbindt <wesselbindt@gmail.com>
2021-04-27 23:06:36 +0200

    Add flag for no spaces

--- xkcd_pass.py
+++ xkcd_pass.py
@@ -3,19 +3,24 @@ from argparse import ArgumentParser
 import secrets
 
 
-def generate(n: int) -> str:
+def generate(n: int, spaces: bool = True) -> str:
     with open('/usr/share/dict/words') as dict_file:
         words = [
             word
             for word in map(lambda x: x.strip(), dict_file)
             if word.isalpha()
         ]
-    return ' '.join(secrets.choice(words) for _ in range(n))
+    separator = ' ' if spaces else ''
+    return separator.join(secrets.choice(words) for _ in range(n))
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('words', type=int)
+    parser.add_argument('--no-spaces', action='store_true')
     args = parser.parse_args()
-    print(generate(args.words))
+    print(generate(
+        args.words, 
+        not args.no_spaces
+    ))
 
